54b10d730dd9d6ddc15e48d52751274930784a32
DanielMesaGonzalez <dmg00040@red.ujaen.es>
2018-02-20 09:57:26 +0100

    primera subida del proyecto

---
+++ Practica1.2/src/Practica1_2/cookie1.java
@@ -0,0 +1,63 @@
+package Practica1_2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class cookie1
+ */
+@WebServlet("/cookie1")
+public class cookie1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public cookie1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession sesion = request.getSession(true);
+		response.setContentType("text/html");
+		PrintWriter html = response.getWriter();
+		html.print("<strong>");
+		Date date = (Date)sesion.getAttribute("date");
+		if(date != null) {
+			
+		            html.print("Último acceso de la sesión: " + date + "<br>");
+		}
+		
+		else {
+			
+		            html.print("Este es el primer acceso de la sesión <br>");
+		            
+		}
+		      date = new Date();
+		      sesion.setAttribute("date", date);
+		      html.print("Fecha actual: " + date);
+		      html.print("</strong>");
+		}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
